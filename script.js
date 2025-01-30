07eaeae841ae261ebce0fb2afd2fa027eb30f502
OhMarcel <mfloreszaher@gmail.com.noreply.github.com>
2025-01-29 20:56:05 -0600

    changed getComputerChoice, made randomVal only output integers, instead of floating points

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-console.log("Hello World");
+console.log(getComputerChoice());
 
 function getComputerChoice(){
     let randomVal = 0;
-    randomVal = Math.random() * (max - min) + min;
+    let max = 2;
+    let min = 0;
+    randomVal = Math.round(Math.random() * (max - min) + min);
     if(randomVal===0){
         return "Rock";
     } else if(randomVal===1){
@@ -10,6 +12,7 @@ function getComputerChoice(){
     } else if(randomVal===2){
         return "Scissors";
     } else{
+        console.log(randomVal);
         return "Computer was not able to ouput a choice";
     }
     return "";
